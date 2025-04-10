d5fb282fbaaa5c008749a393b013a42251a81a67
Matt Oltmanns <moltmanns25@hotmail.com>
2025-04-09 22:25:02 -0500

    Added routes and tabs

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,14 +1,9 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
-
 const tintColorLight = '#0a7ea4';
 const tintColorDark = '#fff';
 
 export const Colors = {
   light: {
-    text: '#11181C',
+    text: '#0d0d0d',
     background: '#fff',
     tint: tintColorLight,
     icon: '#687076',
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}","./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0D0D0D',
+        secondary: '#43DDFF',
+        borderaccent: '#848484',
+        cardborder: '#E1E1E1'
+      }
+    },
+  },
+  plugins: [],
+}
